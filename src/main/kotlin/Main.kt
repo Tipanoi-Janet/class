b797e6d31f29078e3a3d99e444ad46df4b272092
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-04-23 20:11:38 +0300

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,33 @@
-import javax.print.attribute.standard.PrinterInfo
-
-fun main() {
-    var cars = Cars("subaru","forester","kcw23",20)
-    cars.hoot()
-
-    var total= Human("Janet",20,30)
-    total.eat(2)
-    total.speak("Annet is sleeping")
-    total.birthday()
 
 
-}
+
+
 class Cars( var make:String, var model:String, var registration:String,var speed:Int){
     fun hoot (){
         println("pip pip")
     }
 
 }
-class Human(var name:String, var age:Int,var weight:Int ){
-    fun eat(foodWeight:Int){
-        var newWeight= weight +foodWeight
-        println("I am eating $newWeight kgs of food")
-
+class Doctor(name: String, age: Int): Person(name, age){
+    fun treatPatient(patient: String, disease: String){
+        println("Treat $patient for $disease")
     }
-    fun speak(speech:String){
-        println(speech)
+}
+class Farmer(name: String, age: Int): Person(name, age){
+    fun cultivateLand(){
+        println("dig dig dig")
     }
-    fun birthday(){
-        println(++age)
+}
+open class Person(var name: String, var age: Int){
+    fun talk(words: String){
+        println(words)
     }
-
+    fun eat(){
+        println("yum")
     }
-
+    fun sleep(){
+        println("zzzzz")
+    }
+}
 
 
